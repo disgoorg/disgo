1b385defe87bb9cb94431df4421339bcccddd4c6
TopiSenpai <tobi@strasserhof.com>
2021-05-01 18:52:23 +0200

    add button ack InteractionResponseType

--- api/interaction_response.go
+++ api/interaction_response.go
@@ -10,7 +10,7 @@ const (
 	_
 	InteractionResponseTypeChannelMessageWithSource
 	InteractionResponseTypeDeferredChannelMessageWithSource
-	_
+	InteractionResponseTypeButtonAck
 	InteractionResponseTypeButtonResponse
 )
 
