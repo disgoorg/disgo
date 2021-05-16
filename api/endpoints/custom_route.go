efa557bffe750c9c80972230d7156fc0e556b35f
TopiSenpai <tobi@strasserhof.com>
2021-05-16 03:40:13 +0200

    updated custom route

--- api/endpoints/custom_route.go
+++ api/endpoints/custom_route.go
@@ -2,12 +2,12 @@ package endpoints
 
 // CustomRoute is APIRoute but custom for you
 type CustomRoute struct {
-	APIRoute
+	*APIRoute
 }
 
 // Compile returns a CompiledAPIRoute
-func (r CustomRoute) Compile(args ...interface{}) (*CompiledAPIRoute, error) {
-	compiledRoute, err := r.Route.Compile(args...)
+func (r CustomRoute) Compile(queryParams map[string]string, args ...interface{}) (*CompiledAPIRoute, error) {
+	compiledRoute, err := r.Route.Compile(queryParams, args...)
 	if err != nil {
 		return nil, err
 	}
@@ -18,12 +18,18 @@ func (r CustomRoute) Compile(args ...interface{}) (*CompiledAPIRoute, error) {
 }
 
 // NewCustomRoute generates a new custom route struct
-func NewCustomRoute(method Method, url string) APIRoute {
-	return APIRoute{
-		Route: Route{
-			baseRoute:  "",
-			route:      url,
-			paramCount: countParams(url),
+func NewCustomRoute(method Method, url string, queryParams ...string) *APIRoute {
+	params := map[string]struct{}{}
+	for _, param := range queryParams {
+		params[param] = struct{}{}
+	}
+
+	return &APIRoute{
+		Route: &Route{
+			baseRoute:   "",
+			route:       url,
+			queryParams: params,
+			paramCount:  countParams(url),
 		},
 		method: method,
 	}
