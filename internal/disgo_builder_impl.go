31f9c178f923ac3bdb81d0493295fdfef04d8c50
TopiSenpai <tobi@strasserhof.com>
2021-06-13 17:42:45 +0200

    forgot to add those to the interface lol

--- internal/disgo_builder_impl.go
+++ internal/disgo_builder_impl.go
@@ -142,20 +142,20 @@ func (b *DisgoBuilderImpl) SetMessageCachePolicy(messageCachePolicy api.MessageC
 }
 
 // SetCacheFlags lets you set the api.CacheFlags
-func (b *DisgoBuilderImpl) SetCacheFlags(cacheFlags api.CacheFlags) api.DisgoBuilder {
-	b.cacheFlags = cacheFlags
+func (b *DisgoBuilderImpl) SetCacheFlags(cacheFlags ...api.CacheFlags) api.DisgoBuilder {
+	b.cacheFlags = api.CacheFlagsNone.Add(cacheFlags...)
 	return b
 }
 
 // EnableCacheFlags lets you enable certain api.CacheFlags
-func (b *DisgoBuilderImpl) EnableCacheFlags(cacheFlags api.CacheFlags) api.DisgoBuilder {
-	b.cacheFlags.Add(cacheFlags)
+func (b *DisgoBuilderImpl) EnableCacheFlags(cacheFlags ...api.CacheFlags) api.DisgoBuilder {
+	b.cacheFlags.Add(cacheFlags...)
 	return b
 }
 
 // DisableCacheFlags lets you disable certain api.CacheFlags
-func (b *DisgoBuilderImpl) DisableCacheFlags(cacheFlags api.CacheFlags) api.DisgoBuilder {
-	b.cacheFlags.Remove(cacheFlags)
+func (b *DisgoBuilderImpl) DisableCacheFlags(cacheFlags ...api.CacheFlags) api.DisgoBuilder {
+	b.cacheFlags.Remove(cacheFlags...)
 	return b
 }
 
