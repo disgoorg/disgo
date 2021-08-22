38d4a260f53d177c376e785684508d88d2e07a01
TopiSenpai <tobi@strasserhof.com>
2021-08-23 01:41:47 +0200

    fixed imports

--- disgo.go
+++ disgo.go
@@ -9,10 +9,10 @@ import (
 	_ "github.com/DisgoOrg/disgo/gateway/handlers"
 	_ "github.com/DisgoOrg/disgo/httpserver"
 	_ "github.com/DisgoOrg/disgo/httpserver/handlers"
+	_ "github.com/DisgoOrg/disgo/info"
 	_ "github.com/DisgoOrg/disgo/oauth2"
 	_ "github.com/DisgoOrg/disgo/rest"
 	_ "github.com/DisgoOrg/disgo/rest/rate"
 	_ "github.com/DisgoOrg/disgo/rest/route"
-	_ "github.com/DisgoOrg/disgo/util"
 	_ "github.com/DisgoOrg/disgo/webhook"
 )
