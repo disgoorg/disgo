f044a56572db95243fd9eca66871758bbf03ec66
TopiSenpai <tobi@strasserhof.com>
2021-09-01 21:44:07 +0200

    fixed some example stuff

--- _examples/oauth2/example.go
+++ _examples/oauth2/example.go
@@ -14,6 +14,7 @@ import (
 var (
 	clientID     = discord.Snowflake(os.Getenv("client_id"))
 	clientSecret = os.Getenv("client_secret")
+	baseURL      = os.Getenv("base_url")
 	logger       = log.Default()
 	httpClient   = http.DefaultClient
 	client       oauth2.Client
@@ -31,13 +32,13 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
 	mux.HandleFunc("/login", handleLogin)
-	mux.HandleFunc("/login", handleLogin)
+	mux.HandleFunc("/trylogin", handleTryLogin)
 	http.ListenAndServe(":6969", mux)
 }
 
-func handleRoot(w http.ResponseWriter, request *http.Request) {
+func handleRoot(w http.ResponseWriter, r *http.Request) {
 	var body string
-	cookie, err := request.Cookie("token")
+	cookie, err := r.Cookie("token")
 	if err != nil {
 		body = `<button href="/login">login</button>`
 	} else {
@@ -49,6 +50,24 @@ func handleRoot(w http.ResponseWriter, request *http.Request) {
 	_, _ = w.Write([]byte(body))
 }
 
-func handleLogin(w http.ResponseWriter, request *http.Request) {
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, client.GenerateAuthorizationURL(baseURL+"/trylogin"), http.StatusMovedPermanently)
+}
 
+func handleTryLogin(w http.ResponseWriter, r *http.Request) {
+	var (
+		query = r.URL.Query()
+		code  = query.Get("code")
+		state = query.Get("state")
+	)
+	if code != "" && state != "" {
+		session, err := client.StartSession(code, state, oauth2.RandStr(32))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("error while getting session: " + err.Error()))
+			return
+		}
+		client.
+	}
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
