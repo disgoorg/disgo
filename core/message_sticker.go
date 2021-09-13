64ece318b1957157dbdc8f2ad17d1825257f9e86
TopiSenpai <tobi@strasserhof.com>
2021-09-14 01:20:19 +0200

    added MessageSticker#URL

--- core/message_sticker.go
+++ core/message_sticker.go
@@ -3,11 +3,21 @@ package core
 import (
 	"github.com/DisgoOrg/disgo/discord"
 	"github.com/DisgoOrg/disgo/rest"
+	"github.com/DisgoOrg/disgo/rest/route"
 )
 
 type MessageSticker struct {
 	discord.MessageSticker
 	Bot *Bot
+}
+
+func (s *MessageSticker) URL(size int) string {
+	fileExtension := route.PNG
+	if s.FormatType == discord.StickerFormatTypeLottie {
+		fileExtension = route.Lottie
+	}
+	compiledRoute, _ := route.CustomSticker.Compile(nil, fileExtension, size, s.ID)
+	return compiledRoute.URL()
 }
 
 func (s *MessageSticker) GetSticker() (*Sticker, rest.Error) {
