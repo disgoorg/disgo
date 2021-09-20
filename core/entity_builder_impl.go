cd84458c333be21a324a9cb2f1db4d597a151f89
TopiSenpai <tobi@strasserhof.com>
2021-09-20 19:31:36 +0200

    thx discord

--- core/entity_builder_impl.go
+++ core/entity_builder_impl.go
@@ -30,11 +30,10 @@ func (b *entityBuilderImpl) CreateInteraction(interaction discord.Interaction, c
 	}
 
 	if interaction.Member != nil {
-		interaction.Member.User = interaction.User // fuck u discord why not give the user here
 		coreInteraction.Member = b.CreateMember(*interaction.GuildID, *interaction.Member, updateCache)
 		coreInteraction.User = coreInteraction.Member.User
 	} else {
-		coreInteraction.User = b.CreateUser(interaction.User, updateCache)
+		coreInteraction.User = b.CreateUser(*interaction.User, updateCache)
 	}
 
 	return coreInteraction
