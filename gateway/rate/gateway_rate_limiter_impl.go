236ad74323bdfcc9b2b5ea94e6901919eb3c4acf
TopiSenpai <tobi@strasserhof.com>
2021-09-26 17:50:26 +0200

    fixed gateway ratelimiter blocking one min after frist init

--- gateway/rate/gateway_rate_limiter_impl.go
+++ gateway/rate/gateway_rate_limiter_impl.go
@@ -31,34 +31,30 @@ type limiterImpl struct {
 	config Config
 }
 
-func (r *limiterImpl) Logger() log.Logger {
-	return r.config.Logger
+func (l *limiterImpl) Logger() log.Logger {
+	return l.config.Logger
 }
 
-func (r *limiterImpl) Close(ctx context.Context) {
+func (l *limiterImpl) Close(ctx context.Context) {
 	// TODO: wait for all buckets to unlock
 }
 
-func (r *limiterImpl) Config() Config {
-	return r.config
+func (l *limiterImpl) Config() Config {
+	return l.config
 }
 
-func (r *limiterImpl) Wait(ctx context.Context) error {
-	r.Logger().Debug("locking gateway rate limiter")
-	if err := r.CLock(ctx); err != nil {
+func (l *limiterImpl) Wait(ctx context.Context) error {
+	l.Logger().Debug("locking gateway rate limiter")
+	if err := l.CLock(ctx); err != nil {
 		return err
 	}
 
 	now := time.Now()
 
-	if r.reset.IsZero() {
-		r.reset = now.Add(time.Minute)
-	}
-
 	var until time.Time
 
-	if r.remaining == 0 && r.reset.After(now) {
-		until = r.reset
+	if l.remaining == 0 && l.reset.After(now) {
+		until = l.reset
 	}
 
 	if until.After(now) {
@@ -69,7 +65,7 @@ func (r *limiterImpl) Wait(ctx context.Context) error {
 
 		select {
 		case <-ctx.Done():
-			r.Unlock()
+			l.Unlock()
 			return ctx.Err()
 		case <-time.After(until.Sub(now)):
 		}
@@ -77,12 +73,12 @@ func (r *limiterImpl) Wait(ctx context.Context) error {
 	return nil
 }
 
-func (r *limiterImpl) Unlock() {
-	r.Logger().Debug("unlocking gateway rate limiter")
+func (l *limiterImpl) Unlock() {
+	l.Logger().Debug("unlocking gateway rate limiter")
 	now := time.Now()
-	if r.reset.Before(now) {
-		r.reset = now.Add(time.Minute)
-		r.remaining = r.Config().CommandsPerMinute
+	if l.reset.Before(now) {
+		l.reset = now.Add(time.Minute)
+		l.remaining = l.Config().CommandsPerMinute
 	}
-	r.Mutex.Unlock()
+	l.Mutex.Unlock()
 }
