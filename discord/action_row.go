fb4bb75295bc72f43a0bb3c1218976b36a7d2466
TopiSenpai <tobi@strasserhof.com>
2021-09-28 01:51:39 +0200

    removed unused discord type

--- discord/action_row.go
+++
@@ -1,7 +0,0 @@
-package discord
-
-// ActionRow holds up to 5 Component(s) in a row
-type ActionRow struct {
-	Type       ComponentType `json:"type"`
-	Components []Component   `json:"components"`
-}
