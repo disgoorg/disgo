b06ecd2bd21af8198b53e539fc2d4f0b5bdbe4ee
TopiSenpai <tobi@strasserhof.com>
2021-10-09 01:09:14 +0200

    remove intents for test bot

--- _examples/ping_pong/example.go
+++ _examples/ping_pong/example.go
@@ -20,9 +20,7 @@ func main() {
 	disgo, err := bot.New(os.Getenv("token"),
 		bot.WithGatewayOpts(
 			gateway.WithGatewayIntents(
-				discord.GatewayIntentGuilds,
-				discord.GatewayIntentGuildMessages,
-				discord.GatewayIntentDirectMessages,
+				discord.GatewayIntentsNone,
 			),
 		),
 		bot.WithCacheOpts(core.WithCacheFlags(core.CacheFlagsDefault)),
