5027416807de884e9a51990895647d2151e29b36
Skye <46286597+Skye-31@users.noreply.github.com>
2022-03-29 13:03:05 +0100

    fix(GuildCreate): add guild to cache

--- gateway/handlers/guild_create_handler.go
+++ gateway/handlers/guild_create_handler.go
@@ -28,6 +28,8 @@ func (h *gatewayHandlerGuildCreate) HandleGatewayEvent(client bot.Client, sequen
 
 	wasUnready := client.Caches().Guilds().IsUnready(shardID, gatewayGuild.ID)
 	wasUnavailable := client.Caches().Guilds().IsUnavailable(gatewayGuild.ID)
+	
+	client.Caches().Guilds().Put(gatewayGuild.ID, gatewayGuild)
 
 	for _, channel := range gatewayGuild.Channels {
 		// populate unset field
