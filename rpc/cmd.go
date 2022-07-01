97bab68530dad8c1ce584e49a50b2e0d44cf9367
TopiSenpai <tobi@strasserhof.com>
2022-07-01 19:21:27 +0200

    event & cmd payloads

--- rpc/cmd.go
+++ rpc/cmd.go
@@ -37,6 +37,259 @@ type CmdRsAuthenticate struct {
 
 func (CmdRsAuthenticate) messageData() {}
 
+type CmdArgsGetGuild struct {
+	GuildID snowflake.ID `json:"guild_id"`
+	Timeout int          `json:"timeout"`
+}
+
+func (CmdArgsGetGuild) cmdArgs() {}
+
+type PartialGuild struct {
+	ID      snowflake.ID `json:"id"`
+	Name    string       `json:"name"`
+	IconURL *string      `json:"icon_url,omitempty"`
+}
+
+type CmdRsGetGuild struct {
+	PartialGuild
+}
+
+func (CmdRsGetGuild) messageData() {}
+
+type CmdRsGetGuilds struct {
+	Guilds []PartialGuild `json:"guilds"`
+}
+
+func (CmdRsGetGuilds) messageData() {}
+
+type CmdArgsGetChannel struct {
+	ChannelID snowflake.ID `json:"channel_id"`
+}
+
+func (CmdArgsGetChannel) cmdArgs() {}
+
+type PartialChannel struct {
+	ID          snowflake.ID        `json:"id"`
+	GuildID     *snowflake.ID       `json:"guild_id,omitempty"`
+	Name        string              `json:"name"`
+	Type        discord.ChannelType `json:"type"`
+	Topic       *string             `json:"topic,omitempty"`
+	Bitrate     int                 `json:"bitrate,omitempty"`
+	UserLimit   int                 `json:"user_limit,omitempty"`
+	Position    int                 `json:"position,omitempty"`
+	VoiceStates []VoiceState        `json:"voice_states,omitempty"`
+	Messages    []Message           `json:"messages,omitempty"`
+}
+
+type VoiceState struct {
+	discord.VoiceState
+	Volume int `json:"volume"`
+	Pan    Pan `json:"pan"`
+}
+
+type Pan struct {
+	Left  float32 `json:"left"`
+	Right float32 `json:"right"`
+}
+
+type CmdRsGetChannel struct {
+	PartialChannel
+}
+
+func (CmdRsGetChannel) messageData() {}
+
+type CmdArgsGetChannels struct {
+	GuildID snowflake.ID `json:"guild_id"`
+}
+
+func (CmdArgsGetChannels) cmdArgs() {}
+
+type CmdRsGetChannels struct {
+	Channels []PartialChannel `json:"channels"`
+}
+
+func (CmdRsGetChannels) messageData() {}
+
+type CmdArgsSetUserVoiceSettings struct {
+	UserID snowflake.ID `json:"user_id"`
+	Pain   *Pan         `json:"pain"`
+	Volume *int         `json:"volume"`
+	Mute   *bool        `json:"mute"`
+}
+
+func (CmdArgsSetUserVoiceSettings) cmdArgs() {}
+
+type CmdRsSetUserVoiceSettings struct {
+	UserID snowflake.ID `json:"user_id"`
+	Pain   Pan          `json:"pain"`
+	Volume int          `json:"volume"`
+	Mute   bool         `json:"mute"`
+}
+
+func (CmdRsSetUserVoiceSettings) messageData() {}
+
+type CmdArgsSelectVoiceChannel struct {
+	ChannelID snowflake.ID `json:"channel_id"`
+	Timeout   int          `json:"timeout"`
+	Force     bool         `json:"force"`
+}
+
+func (CmdArgsSelectVoiceChannel) cmdArgs() {}
+
+type CmdRsSelectVoiceChannel struct {
+	PartialChannel
+}
+
+func (CmdRsSelectVoiceChannel) messageData() {}
+
+type CmdRsGetSelectedVoiceChannel struct {
+	*PartialChannel
+}
+
+func (CmdRsGetSelectedVoiceChannel) messageData() {}
+
+type CmdArgsSelectTextChannel struct {
+	ChannelID *snowflake.ID `json:"channel_id"`
+	Timeout   int           `json:"timeout"`
+}
+
+func (CmdArgsSelectTextChannel) cmdArgs() {}
+
+type CmdRsSelectTextChannel struct {
+	*PartialChannel
+}
+
+func (CmdRsSelectTextChannel) messageData() {}
+
+type CmdRsGetVoiceSettings struct {
+	VoiceSettings
+}
+
+type VoiceSettings struct {
+	Input                VoiceSettingsIO   `json:"input"`
+	Output               VoiceSettingsIO   `json:"output"`
+	Mode                 VoiceSettingsMode `json:"mode"`
+	AutomaticGainControl bool              `json:"automatic_gain_control"`
+	EchoCancellation     bool              `json:"echo_cancellation"`
+	NoiseSuppression     bool              `json:"noise_suppression"`
+	QOS                  bool              `json:"qos"`
+	SilenceWarning       bool              `json:"silence_warning"`
+	Deaf                 bool              `json:"deaf"`
+	Mute                 bool              `json:"mute"`
+}
+
+type Device struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type VoiceSettingsIO struct {
+	DeviceID         string   `json:"device_id"`
+	Volume           int      `json:"volume"`
+	AvailableDevices []Device `json:"available_devices"`
+}
+
+type VoiceSettingsModeType string
+
+const (
+	VoiceSettingsModeTypePushToTalk VoiceSettingsModeType = "PUSH_TO_TALK"
+	VoiceSettingsModeTypeActivity   VoiceSettingsModeType = "ACTIVITY"
+)
+
+type VoiceSettingsMode struct {
+	Type          VoiceSettingsModeType `json:"type"`
+	AutoThreshold bool                  `json:"auto_threshold"`
+	Threshold     int                   `json:"threshold"`
+	Shortcut      ShortcutKeyCombo      `json:"shortcut"`
+	Delay         float32               `json:"delay"`
+}
+
+type ShortcutKeyComboType int
+
+const (
+	ShortcutKeyComboTypeKeyboardKey ShortcutKeyComboType = iota
+	ShortcutKeyComboTypeMouseButton
+	ShortcutKeyComboTypeModifierKey
+	ShortcutKeyComboTypeGamepadButton
+)
+
+type ShortcutKeyCombo struct {
+	Type ShortcutKeyComboType `json:"type"`
+	Code int                  `json:"code"`
+	Name string               `json:"name"`
+}
+
+func (CmdRsGetVoiceSettings) messageData() {}
+
+type CmdArgsSetVoiceSettings struct {
+	Input                *VoiceSettings     `json:"input"`
+	Output               *VoiceSettings     `json:"output"`
+	Mode                 *VoiceSettingsMode `json:"mode"`
+	AutomaticGainControl *bool              `json:"automatic_gain_control"`
+	EchoCancellation     *bool              `json:"echo_cancellation"`
+	NoiseSuppression     *bool              `json:"noise_suppression"`
+	QOS                  *bool              `json:"qos"`
+	SilenceWarning       *bool              `json:"silence_warning"`
+	Deaf                 *bool              `json:"deaf"`
+	Mute                 *bool              `json:"mute"`
+}
+
+func (CmdArgsSetVoiceSettings) cmdArgs() {}
+
+type CmdRsSetVoiceSettings struct {
+	VoiceSettings
+}
+
+func (CmdRsSetVoiceSettings) messageData() {}
+
+type DeviceType string
+
+const (
+	DeviceTypeAudioInput  DeviceType = "audioinput"
+	DeviceTypeAudioOutput DeviceType = "audiooutput"
+	DeviceTypeVideoInput  DeviceType = "videoinput"
+)
+
+type DeviceVendor struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type DeviceModel struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type CertifiedDevice struct {
+	Type                 DeviceType   `json:"type"`
+	ID                   string       `json:"id"`
+	Vendor               DeviceVendor `json:"vendor"`
+	Model                DeviceModel  `json:"model"`
+	Related              []string     `json:"related"`
+	EchoCancellation     bool         `json:"echo_cancellation"`
+	NoiseSuppression     bool         `json:"noise_suppression"`
+	AutomaticGainControl bool         `json:"automatic_gain_control"`
+	HardwareMute         bool         `json:"hardware_mute"`
+}
+
+type CmdArgsSetCertifiedDevices struct {
+	Devices []CertifiedDevice `json:"devices"`
+}
+
+func (CmdArgsSetCertifiedDevices) cmdArgs() {}
+
+type CmdArgsSendActivityJoinInvite struct {
+	UserID snowflake.ID `json:"user_id"`
+}
+
+func (CmdArgsSendActivityJoinInvite) cmdArgs() {}
+
+type CmdArgsCloseActivityRequest struct {
+	UserID snowflake.ID `json:"user_id"`
+}
+
+func (CmdArgsCloseActivityRequest) cmdArgs() {}
+
 type CmdArgsSetActivity struct {
 	PID      int              `json:"pid"`
 	Activity discord.Activity `json:"activity"`
@@ -62,19 +315,22 @@ type CmdArgsSubscribeMessage struct {
 func (CmdArgsSubscribeMessage) cmdArgs()          {}
 func (CmdArgsSubscribeMessage) cmdArgsSubscribe() {}
 
+type CmdArgsSubscribeGuild struct {
+	GuildID snowflake.ID `json:"guild_id"`
+}
+
+func (CmdArgsSubscribeGuild) cmdArgs()          {}
+func (CmdArgsSubscribeGuild) cmdArgsSubscribe() {}
+
+type CmdArgsSubscribeSpeaking struct {
+	ChannelID snowflake.ID `json:"channel_id"`
+}
+
+func (CmdArgsSubscribeSpeaking) cmdArgs()          {}
+func (CmdArgsSubscribeSpeaking) cmdArgsSubscribe() {}
+
 type CmdRsSubscribe struct {
 	Evt string `json:"evt"`
 }
 
 func (CmdRsSubscribe) messageData() {}
-
-type CmdArgsUnsubscribe struct {
-}
-
-func (CmdArgsUnsubscribe) cmdArgs() {}
-
-type CmdRsUnsubscribe struct {
-	Evt string `json:"evt"`
-}
-
-func (CmdRsUnsubscribe) messageData() {}
--- rpc/event.go
+++ rpc/event.go
@@ -7,6 +7,12 @@ import (
 	"github.com/disgoorg/snowflake/v2"
 )
 
+type ServerConfig struct {
+	CDNHost     string `json:"cdn_host"`
+	APIEndpoint string `json:"api_endpoint"`
+	Environment string `json:"environment"`
+}
+
 type EventDataReady struct {
 	V      int          `json:"v"`
 	Config ServerConfig `json:"config"`
@@ -15,11 +21,95 @@ type EventDataReady struct {
 
 func (EventDataReady) messageData() {}
 
-type ServerConfig struct {
-	CDNHost     string `json:"cdn_host"`
-	APIEndpoint string `json:"api_endpoint"`
-	Environment string `json:"environment"`
+var _ error = (*EventDataError)(nil)
+
+type EventDataError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
 }
+
+func (e EventDataError) Error() string {
+	return fmt.Sprintf("%d: %s", e.Code, e.Message)
+}
+
+func (EventDataError) messageData() {}
+
+type EventDataGuildStatus struct {
+	Guild PartialGuild `json:"guild"`
+}
+
+func (EventDataGuildStatus) messageData() {}
+
+type EventDataGuildCreate struct {
+	ID   snowflake.ID `json:"id"`
+	Name string       `json:"name"`
+}
+
+func (EventDataGuildCreate) messageData() {}
+
+type EventDataChannelCreate struct {
+	ID   snowflake.ID        `json:"id"`
+	Name string              `json:"name"`
+	Type discord.ChannelType `json:"type"`
+}
+
+func (EventDataChannelCreate) messageData() {}
+
+type EventDataSelectVoiceChannel struct {
+	ChannelID *snowflake.ID `json:"channel_id"`
+	GuildID   *snowflake.ID `json:"guild_id"`
+}
+
+func (EventDataSelectVoiceChannel) messageData() {}
+
+type EventDataVoiceSettingsUpdate struct {
+	VoiceSettings
+}
+
+func (EventDataVoiceSettingsUpdate) messageData() {}
+
+type EventDataVoiceStateCreate struct {
+	VoiceState
+}
+
+func (EventDataVoiceStateCreate) messageData() {}
+
+type EventDataVoiceStateUpdate struct {
+	VoiceState
+}
+
+func (EventDataVoiceStateUpdate) messageData() {}
+
+type EventDataEventDataVoiceStateDelete struct {
+	VoiceState
+}
+
+func (EventDataEventDataVoiceStateDelete) messageData() {}
+
+type VoiceStateType string
+
+const (
+	VoiceStateTypeDisconnected      VoiceStateType = "DISCONNECTED"
+	VoiceStateTypeAwaitingEndpoint  VoiceStateType = "AWAITING_ENDPOINT"
+	VoiceStateTypeAuthenticating    VoiceStateType = "AUTHENTICATING"
+	VoiceStateTypeConnecting        VoiceStateType = "CONNECTING"
+	VoiceStateTypeConnected         VoiceStateType = "CONNECTED"
+	VoiceStateTypeVoiceDisconnected VoiceStateType = "VOICE_DISCONNECTED"
+	VoiceStateTypeVoiceConnecting   VoiceStateType = "VOICE_CONNECTING"
+	VoiceStateTypeVoiceConnected    VoiceStateType = "VOICE_CONNECTED"
+	VoiceStateTypeNoRoute           VoiceStateType = "NO_ROUTE"
+	VoiceStateTypeICEChecking       VoiceStateType = "ICE_CHECKING"
+)
+
+type EventDataVoiceConnectionStatus struct {
+	State       VoiceStateType `json:"state"`
+	Hostname    string         `json:"hostname"`
+	Pings       []float32      `json:"pings"`
+	AveragePing float32        `json:"average_ping"`
+	LastPing    float32        `json:"last_ping"`
+}
+
+func (EventDataVoiceConnectionStatus) messageData() {}
 
 type EventDataMessageCreate struct {
 	ChannelID snowflake.ID    `json:"channel_id"`
@@ -44,15 +134,42 @@ type EventDataMessageDelete struct {
 
 func (EventDataMessageDelete) messageData() {}
 
-var _ error = (*EventDataError)(nil)
-
-type EventDataError struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+type EventDataSpeakingStart struct {
+	UserID snowflake.ID `json:"user_id"`
 }
 
-func (e EventDataError) Error() string {
-	return fmt.Sprintf("%d: %s", e.Code, e.Message)
+func (EventDataSpeakingStart) messageData() {}
+
+type EventDataSpeakingStop struct {
+	UserID snowflake.ID `json:"user_id"`
 }
 
-func (EventDataError) messageData() {}
+func (EventDataSpeakingStop) messageData() {}
+
+type EventDataNotificationCreate struct {
+	ChannelID snowflake.ID    `json:"channel_id"`
+	Message   discord.Message `json:"message"`
+	IconURL   string          `json:"icon_url"`
+	Title     string          `json:"title"`
+	Body      string          `json:"body"`
+}
+
+func (EventDataNotificationCreate) messageData() {}
+
+type EventDataActivityJoin struct {
+	Secret string `json:"secret"`
+}
+
+func (EventDataActivityJoin) messageData() {}
+
+type EventDataActivitySpectate struct {
+	Secret string `json:"secret"`
+}
+
+func (EventDataActivitySpectate) messageData() {}
+
+type EventDataActivityJoinRequest struct {
+	User discord.User `json:"user"`
+}
+
+func (EventDataActivityJoinRequest) messageData() {}
