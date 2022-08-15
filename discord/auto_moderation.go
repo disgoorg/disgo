26892aea07d32c262ab741d5d54168ee304cbfc8
caneleex <me@mlnr.dev>
2022-08-16 00:03:54 +0200

    remove AutoModerationTriggerTypeHarmfulLink

--- discord/auto_moderation.go
+++ discord/auto_moderation.go
@@ -12,7 +12,7 @@ type AutoModerationTriggerType int
 
 const (
 	AutoModerationTriggerTypeKeyword AutoModerationTriggerType = iota + 1
-	AutoModerationTriggerTypeHarmfulLink
+	_
 	AutoModerationTriggerTypeSpam
 	AutoModerationTriggerTypeKeywordPresent
 	AutoModerationTriggerTypeMentionSpam
