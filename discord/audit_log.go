e93678014453be3e5dec09353b70c565ed8e3ae7
caneleex <me@mlnr.dev>
2023-01-18 14:00:00 +0100

    make GuildID a pointer

--- discord/audit_log.go
+++ discord/audit_log.go
@@ -173,7 +173,7 @@ type AuditLogEntry struct {
 	UserID     snowflake.ID               `json:"user_id"`
 	ID         snowflake.ID               `json:"id"`
 	ActionType AuditLogEvent              `json:"action_type"`
-	GuildID    snowflake.ID               `json:"guild_id"`
+	GuildID    *snowflake.ID              `json:"guild_id"`
 	Options    *OptionalAuditLogEntryInfo `json:"options"`
 	Reason     *string                    `json:"reason"`
 }
