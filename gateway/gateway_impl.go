39a4e550b6134ef7c38a9993417ca59a7afb890e
ZenShibata <zen@ayato.life>
2023-03-29 17:58:00 +0700

    feat(events): handle heartbeat ack events * Added event payload: LastHeartbeat & NewHeartbeat

--- gateway/gateway_impl.go
+++ gateway/gateway_impl.go
@@ -448,8 +448,12 @@ loop:
 			break loop
 
 		case OpcodeHeartbeatACK:
-			g.lastHeartbeatReceived = time.Now().UTC()
-			g.eventHandlerFunc(EventTypeHeartbeatAck, message.S, g.config.ShardID, nil)
+			newHeartbeat := time.Now().UTC()
+			g.eventHandlerFunc(EventTypeHeartbeatAck, message.S, g.config.ShardID, EventHeartbeatAck{
+				LastHeartbeat: g.lastHeartbeatReceived,
+				NewHeartbeat:  newHeartbeat,
+			})
+			g.lastHeartbeatReceived = newHeartbeat
 
 		default:
 			g.config.Logger.Debug(g.formatLogsf("unknown opcode received: %d, data: %s", message.Op, message.D))
