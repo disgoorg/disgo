6ec38c999b1c0782177ad5c831d41298fc2fc5e1
caneleex <me@mlnr.dev>
2023-04-01 15:09:04 +0200

    Add AuditLogGuildOnboardingUpdate

--- discord/audit_log.go
+++ discord/audit_log.go
@@ -117,6 +117,10 @@ const (
 	AuditLogAutoModerationUserCommunicationDisabled
 )
 
+const (
+	AuditLogGuildOnboardingUpdate AuditLogEvent = 167
+)
+
 // AuditLog (https://discord.com/developers/docs/resources/audit-log) These are logs of events that occurred, accessible via the Discord
 type AuditLog struct {
 	ApplicationCommands  []ApplicationCommand  `json:"application_commands"`
