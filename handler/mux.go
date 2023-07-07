8f83c4507224581733be167474dc37feb8e25f9f
Toπ <git@topi.wtf>
2023-07-08 01:43:12 +0200

    enforce handler patterns to start with /

--- handler/mux.go
+++ handler/mux.go
@@ -166,7 +166,7 @@ func (r *Mux) Autocomplete(pattern string, h AutocompleteHandler) {
 
 // Component registers the given ComponentHandler to the current Router.
 func (r *Mux) Component(pattern string, h ComponentHandler) {
-	checkPatternEmpty(pattern)
+	checkPattern(pattern)
 	r.handle(&handlerHolder[ComponentHandler]{
 		pattern: pattern,
 		handler: h,
@@ -176,7 +176,7 @@ func (r *Mux) Component(pattern string, h ComponentHandler) {
 
 // Modal registers the given ModalHandler to the current Router.
 func (r *Mux) Modal(pattern string, h ModalHandler) {
-	checkPatternEmpty(pattern)
+	checkPattern(pattern)
 	r.handle(&handlerHolder[ModalHandler]{
 		pattern: pattern,
 		handler: h,
@@ -190,14 +190,10 @@ func (r *Mux) NotFound(h NotFoundHandler) {
 	r.notFoundHandler = h
 }
 
-func checkPatternEmpty(pattern string) {
-	if pattern == "" {
+func checkPattern(pattern string) {
+	if len(pattern) == 0 {
 		panic("pattern must not be empty")
 	}
-}
-
-func checkPattern(pattern string) {
-	checkPatternEmpty(pattern)
 	if pattern[0] != '/' {
 		panic("pattern must start with /")
 	}
