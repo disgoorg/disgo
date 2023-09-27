c9986e891d60fb848297678d3af03f7fbf74366f
cane <git@mlnr.dev>
2023-09-27 16:54:59 +0200

    Add default values to select menus (#315)

--- discord/interaction_application_command.go
+++ discord/interaction_application_command.go
@@ -139,7 +139,7 @@ type rawSlashCommandInteractionData struct {
 	Name     string                       `json:"name"`
 	Type     ApplicationCommandType       `json:"type"`
 	GuildID  *snowflake.ID                `json:"guild_id,omitempty"`
-	Resolved SlashCommandResolved         `json:"resolved"`
+	Resolved ResolvedData                 `json:"resolved"`
 	Options  []internalSlashCommandOption `json:"options"`
 }
 
@@ -170,7 +170,7 @@ type SlashCommandInteractionData struct {
 	guildID             *snowflake.ID
 	SubCommandName      *string
 	SubCommandGroupName *string
-	Resolved            SlashCommandResolved
+	Resolved            ResolvedData
 	Options             map[string]SlashCommandOption
 }
 
@@ -497,30 +497,6 @@ func (d SlashCommandInteractionData) FindAll(optionFindFunc func(option SlashCom
 }
 
 func (SlashCommandInteractionData) applicationCommandInteractionData() {}
-
-type SlashCommandResolved struct {
-	Users       map[snowflake.ID]User            `json:"users,omitempty"`
-	Members     map[snowflake.ID]ResolvedMember  `json:"members,omitempty"`
-	Roles       map[snowflake.ID]Role            `json:"roles,omitempty"`
-	Channels    map[snowflake.ID]ResolvedChannel `json:"channels,omitempty"`
-	Attachments map[snowflake.ID]Attachment      `json:"attachments,omitempty"`
-}
-
-func (r *SlashCommandResolved) UnmarshalJSON(data []byte) error {
-	type slashCommandResolved SlashCommandResolved
-	var v slashCommandResolved
-	if err := json.Unmarshal(data, &v); err != nil {
-		return err
-	}
-	*r = SlashCommandResolved(v)
-	for id, member := range r.Members {
-		if user, ok := r.Users[id]; ok {
-			member.User = user
-			r.Members[id] = member
-		}
-	}
-	return nil
-}
 
 type ContextCommandInteractionData interface {
 	ApplicationCommandInteractionData
