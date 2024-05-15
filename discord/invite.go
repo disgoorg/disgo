b9fd973f2ae1d4fe18812b41092478aaabd9c6e9
Sebastian <git@mlnr.dev>
2024-05-15 20:41:25 +0200

    Add InviteType (#327)

--- discord/invite.go
+++ discord/invite.go
@@ -18,6 +18,7 @@ const (
 
 // Invite is a partial invite struct
 type Invite struct {
+	Type                     InviteType           `json:"type"`
 	Code                     string               `json:"code"`
 	Guild                    *InviteGuild         `json:"guild"`
 	Channel                  *InviteChannel       `json:"channel"`
@@ -34,6 +35,14 @@ type Invite struct {
 func (i Invite) URL() string {
 	return InviteURL(i.Code)
 }
+
+type InviteType int
+
+const (
+	InviteTypeGuild InviteType = iota
+	InviteTypeGroupDM
+	InviteTypeFriend
+)
 
 type ExtendedInvite struct {
 	Invite
