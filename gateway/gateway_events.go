9356c13378efbb877efb4cc5e6c20810255ade6c
mlnrDev <git@mlnr.dev>
2024-05-16 12:50:59 +0200

    Add Type to EventMessageReactionAdd/Remove

--- gateway/gateway_events.go
+++ gateway/gateway_events.go
@@ -187,15 +187,16 @@ func (EventGuildAuditLogEntryCreate) messageData() {}
 func (EventGuildAuditLogEntryCreate) eventData()   {}
 
 type EventMessageReactionAdd struct {
-	UserID          snowflake.ID         `json:"user_id"`
-	ChannelID       snowflake.ID         `json:"channel_id"`
-	MessageID       snowflake.ID         `json:"message_id"`
-	GuildID         *snowflake.ID        `json:"guild_id"`
-	Member          *discord.Member      `json:"member"`
-	Emoji           discord.PartialEmoji `json:"emoji"`
-	MessageAuthorID *snowflake.ID        `json:"message_author_id"`
-	BurstColors     []string             `json:"burst_colors"`
-	Burst           bool                 `json:"burst"`
+	UserID          snowflake.ID                `json:"user_id"`
+	ChannelID       snowflake.ID                `json:"channel_id"`
+	MessageID       snowflake.ID                `json:"message_id"`
+	GuildID         *snowflake.ID               `json:"guild_id"`
+	Member          *discord.Member             `json:"member"`
+	Emoji           discord.PartialEmoji        `json:"emoji"`
+	MessageAuthorID *snowflake.ID               `json:"message_author_id"`
+	BurstColors     []string                    `json:"burst_colors"`
+	Burst           bool                        `json:"burst"`
+	Type            discord.MessageReactionType `json:"type"`
 }
 
 func (e *EventMessageReactionAdd) UnmarshalJSON(data []byte) error {
@@ -215,13 +216,14 @@ func (EventMessageReactionAdd) messageData() {}
 func (EventMessageReactionAdd) eventData()   {}
 
 type EventMessageReactionRemove struct {
-	UserID      snowflake.ID         `json:"user_id"`
-	ChannelID   snowflake.ID         `json:"channel_id"`
-	MessageID   snowflake.ID         `json:"message_id"`
-	GuildID     *snowflake.ID        `json:"guild_id"`
-	Emoji       discord.PartialEmoji `json:"emoji"`
-	BurstColors []string             `json:"burst_colors"`
-	Burst       bool                 `json:"burst"`
+	UserID      snowflake.ID                `json:"user_id"`
+	ChannelID   snowflake.ID                `json:"channel_id"`
+	MessageID   snowflake.ID                `json:"message_id"`
+	GuildID     *snowflake.ID               `json:"guild_id"`
+	Emoji       discord.PartialEmoji        `json:"emoji"`
+	BurstColors []string                    `json:"burst_colors"`
+	Burst       bool                        `json:"burst"`
+	Type        discord.MessageReactionType `json:"type"`
 }
 
 func (EventMessageReactionRemove) messageData() {}
