3cab46b7fcee1d5502fcc324f140c385d6b3501e
sebm253 <42180891+sebm253@users.noreply.github.com>
2024-09-21 20:21:37 +0200

    Only append period with extension to proper routes

--- discord/cdn_endpoints.go
+++ discord/cdn_endpoints.go
@@ -94,12 +94,17 @@ func (e CDNEndpoint) URL(format FileFormat, values QueryValues, params ...any) s
 		query = "?" + query
 	}
 
-	// for some reason custom gif stickers use a different cnd url, blame discord for this one
+	// for some reason custom gif stickers use a different cdn url, blame discord for this one
 	if format == FileFormatGIF && e.Route == "/stickers/{sticker.id}" {
 		return urlPrint(CDNMedia+e.Route+"."+format.String(), params...) + query
 	}
+	route := CDN + e.Route
+	// only append period and file extension if the format is not FileFormatNone
+	if format != FileFormatNone {
+		route += "." + format.String()
+	}
 
-	return urlPrint(CDN+e.Route+"."+format.String(), params...) + query
+	return urlPrint(route, params...) + query
 }
 
 func DefaultCDNConfig(format FileFormat) *CDNConfig {
