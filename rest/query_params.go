da7ac5d6cfd947118ce65c378874e2574a22446e
sebm253 <42180891+sebm253@users.noreply.github.com>
2024-12-01 17:23:49 +0100

    add "docs" lol

--- rest/query_params.go
+++ rest/query_params.go
@@ -4,6 +4,8 @@ import (
 	"github.com/disgoorg/disgo/discord"
 )
 
+// QueryParams serves as a generic interface for implementations of rest endpoint query parameters.
 type QueryParams interface {
+	// ToQueryValues transforms fields from the QueryParams interface implementations into discord.QueryValues.
 	ToQueryValues() discord.QueryValues
 }
