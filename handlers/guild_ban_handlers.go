de723e939acd04567ab0503d13976a10b00fe268
apricotbucket28 <agustin.nicolas.marcos@outlook.com>
2025-03-06 20:26:54 -0300

    handlers: Provide full GatewayGuild object on guild create events

--- handlers/guild_ban_handlers.go
+++ handlers/guild_ban_handlers.go
@@ -7,17 +7,25 @@ import (
 )
 
 func gatewayHandlerGuildBanAdd(client bot.Client, sequenceNumber int, shardID int, event gateway.EventGuildBanAdd) {
-	client.EventManager().DispatchEvent(&events.GuildBan{
+	genericGuildEvent := &events.GenericGuild{
 		GenericEvent: events.NewGenericEvent(client, sequenceNumber, shardID),
 		GuildID:      event.GuildID,
+	}
+
+	client.EventManager().DispatchEvent(&events.GuildBan{
+		GenericGuild: genericGuildEvent,
 		User:         event.User,
 	})
 }
 
 func gatewayHandlerGuildBanRemove(client bot.Client, sequenceNumber int, shardID int, event gateway.EventGuildBanRemove) {
-	client.EventManager().DispatchEvent(&events.GuildUnban{
+	genericGuildEvent := &events.GenericGuild{
 		GenericEvent: events.NewGenericEvent(client, sequenceNumber, shardID),
 		GuildID:      event.GuildID,
+	}
+
+	client.EventManager().DispatchEvent(&events.GuildUnban{
+		GenericGuild: genericGuildEvent,
 		User:         event.User,
 	})
 }
