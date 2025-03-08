5376732e2fdd593d83a6ce7b67d91acbb8e81981
apricotbucket28 <71973804+apricotbucket28@users.noreply.github.com>
2025-03-08 17:26:32 -0300

    Remove `VoiceRegion.Vip` (#436)

--- discord/voice_region.go
+++ discord/voice_region.go
@@ -2,10 +2,9 @@ package discord
 
 // VoiceRegion (https://discord.com/developers/docs/resources/voice#voice-region-object)
 type VoiceRegion struct {
-	ID         string       `json:"id"`
-	Name       string       `json:"name"`
-	Vip        bool         `json:"vip"`
-	Optimal    bool         `json:"optimal"`
-	Deprecated bool         `json:"deprecated"`
-	Custom     bool         `json:"custom"`
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	Optimal    bool   `json:"optimal"`
+	Deprecated bool   `json:"deprecated"`
+	Custom     bool   `json:"custom"`
 }
