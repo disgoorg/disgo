plsummers/disgo-wrapper

add support for selects in modals

discord/interaction_modal_submit.go
@@ -92,3 +92,25 @@ func (d ModalSubmitInteractionData) Text(customID CustomID) string {
 	}
 	return ""
 }
+
+func (d ModalSubmitInteractionData) SelectMenuComponent(customID CustomID) (SelectMenuComponent, bool) {
+	if component, ok := d.Component(customID); ok {
+		selectMenuComponent, ok := component.(SelectMenuComponent)
+		return selectMenuComponent, ok
+	}
+	return SelectMenuComponent{}, false
+}
+
+func (d ModalSubmitInteractionData) OptValues(customID CustomID) ([]string, bool) {
+	if selectMenuComponent, ok := d.SelectMenuComponent(customID); ok {
+		return selectMenuComponent.Values, true
+	}
+	return nil, false
+}
+
+func (d ModalSubmitInteractionData) Values(customID CustomID) []string {
+	if text, ok := d.OptValues(customID); ok {
+		return text
+	}
+	return nil
+}

disgoorg/disgo

discord/interaction_modal_submit.go
@@ -152,3 +152,25 @@ func (d ModalSubmitInteractionData) Text(customID string) string {
 	}
 	return ""
 }
+
+func (d ModalSubmitInteractionData) StringSelectMenuComponent(customID string) (StringSelectMenuComponent, bool) {
+	if component, ok := d.Component(customID); ok {
+		selectMenuComponent, ok := component.(StringSelectMenuComponent)
+		return selectMenuComponent, ok
+	}
+	return StringSelectMenuComponent{}, false
+}
+
+func (d ModalSubmitInteractionData) OptStringValues(customID string) ([]string, bool) {
+	if selectMenuComponent, ok := d.StringSelectMenuComponent(customID); ok {
+		return selectMenuComponent.Values, true
+	}
+	return nil, false
+}
+
+func (d ModalSubmitInteractionData) StringValues(customID string) []string {
+	if text, ok := d.OptStringValues(customID); ok {
+		return text
+	}
+	return nil
+}