7a9202a8ff60dac573b382bec8741c725a37be99
topi314 <git@topi.wtf>
2026-02-11 03:01:28 +0100

    remove message create, mesage update, modal create, webhook message create & webhook message update builders & replace with methods on non builder type

--- discord/message_create.go
+++ discord/message_create.go
@@ -1,10 +1,27 @@
 package discord
 
 import (
+	"fmt"
+	"io"
+	"slices"
+
 	"github.com/disgoorg/snowflake/v2"
 )
 
-// MessageCreate is the struct to create a new Message with
+// NewMessageCreate returns a new MessageCreate to create a message without any fields set.
+func NewMessageCreate() MessageCreate {
+	return MessageCreate{}
+}
+
+// NewMessageCreateV2 returns a new MessageCreate with [MessageFlagIsComponentsV2] flag set & allows to directly pass components.
+func NewMessageCreateV2(components ...LayoutComponent) MessageCreate {
+	return MessageCreate{
+		Flags:      MessageFlagIsComponentsV2,
+		Components: components,
+	}
+}
+
+// MessageCreate is the struct to create a new Message with.
 type MessageCreate struct {
 	Nonce            string             `json:"nonce,omitempty"`
 	Content          string             `json:"content,omitempty"`
@@ -23,7 +40,7 @@ type MessageCreate struct {
 
 func (MessageCreate) interactionCallbackData() {}
 
-// ToBody returns the MessageCreate ready for body
+// ToBody returns the MessageCreate ready for body.
 func (m MessageCreate) ToBody() (any, error) {
 	if len(m.Files) > 0 {
 		m.Attachments = parseAttachments(m.Files)
@@ -39,4 +56,282 @@ func (m MessageCreate) ToResponseBody(response InteractionResponse) (any, error)
 		return PayloadWithFiles(response, m.Files...)
 	}
 	return response, nil
+}
+
+// WithContent returns a new MessageCreate with the provided content.
+func (m MessageCreate) WithContent(content string) MessageCreate {
+	m.Content = content
+	return m
+}
+
+// WithContentf returns a new MessageCreate with the formatted content.
+func (m MessageCreate) WithContentf(content string, a ...any) MessageCreate {
+	return m.WithContent(fmt.Sprintf(content, a...))
+}
+
+// WithNonce returns a new MessageCreate with the provided nonce.
+func (m MessageCreate) WithNonce(nonce string) MessageCreate {
+	m.Nonce = nonce
+	return m
+}
+
+// WithEnforceNonce returns a new MessageCreate with the provided enforce nonce setting (use with WithNonce).
+func (m MessageCreate) WithEnforceNonce(enforce bool) MessageCreate {
+	m.EnforceNonce = enforce
+	return m
+}
+
+// WithTTS returns a new MessageCreate with the provided TTS setting.
+func (m MessageCreate) WithTTS(tts bool) MessageCreate {
+	m.TTS = tts
+	return m
+}
+
+// WithEmbeds returns a new MessageCreate with the provided Embed(s).
+func (m MessageCreate) WithEmbeds(embeds ...Embed) MessageCreate {
+	m.Embeds = embeds
+	return m
+}
+
+// WithEmbed returns a new MessageCreate with the provided Embed at the index.
+func (m MessageCreate) WithEmbed(i int, embed Embed) MessageCreate {
+	if len(m.Embeds) > i {
+		m.Embeds = slices.Insert(m.Embeds, i, embed)
+	}
+	return m
+}
+
+// AddEmbeds returns a new MessageCreate with the provided embeds added.
+func (m MessageCreate) AddEmbeds(embeds ...Embed) MessageCreate {
+	m.Embeds = append(m.Embeds, embeds...)
+	return m
+}
+
+// ClearEmbeds returns a new MessageCreate with no embeds.
+func (m MessageCreate) ClearEmbeds() MessageCreate {
+	m.Embeds = []Embed{}
+	return m
+}
+
+// RemoveEmbed returns a new MessageCreate with the embed at the index removed.
+func (m MessageCreate) RemoveEmbed(i int) MessageCreate {
+	if len(m.Embeds) > i {
+		m.Embeds = slices.Delete(slices.Clone(m.Embeds), i, i+1)
+	}
+	return m
+}
+
+// WithComponents returns a new MessageCreate with the provided LayoutComponent(s).
+func (m MessageCreate) WithComponents(components ...LayoutComponent) MessageCreate {
+	m.Components = components
+	return m
+}
+
+// UpdateComponent returns a new MessageCreate with the provided LayoutComponent at the index.
+func (m MessageCreate) UpdateComponent(id int, component LayoutComponent) MessageCreate {
+	for i, cc := range m.Components {
+		if cc.GetID() == id {
+			m.Components = slices.Clone(m.Components)
+			m.Components[i] = component
+			return m
+		}
+	}
+	return m
+}
+
+// AddComponents returns a new MessageCreate with the provided LayoutComponent(s) added.
+func (m MessageCreate) AddComponents(containers ...LayoutComponent) MessageCreate {
+	m.Components = append(m.Components, containers...)
+	return m
+}
+
+// RemoveComponent returns a new MessageCreate with the LayoutComponent at the index removed.
+func (m MessageCreate) RemoveComponent(id int) MessageCreate {
+	for i, cc := range m.Components {
+		if cc.GetID() == id {
+			m.Components = slices.Delete(slices.Clone(m.Components), i, i+1)
+			return m
+		}
+	}
+	return m
+}
+
+// AddActionRow returns a new MessageCreate with a new ActionRowComponent containing the provided InteractiveComponent(s) added.
+func (m MessageCreate) AddActionRow(components ...InteractiveComponent) MessageCreate {
+	m.Components = append(m.Components, NewActionRow(components...))
+	return m
+}
+
+// ClearComponents returns a new MessageCreate with no LayoutComponent(s).
+func (m MessageCreate) ClearComponents() MessageCreate {
+	m.Components = []LayoutComponent{}
+	return m
+}
+
+// WithStickers returns a new MessageCreate with the provided stickers.
+func (m MessageCreate) WithStickers(stickerIds ...snowflake.ID) MessageCreate {
+	m.StickerIDs = stickerIds
+	return m
+}
+
+// AddStickers returns a new MessageCreate with the provided stickers added.
+func (m MessageCreate) AddStickers(stickerIds ...snowflake.ID) MessageCreate {
+	m.StickerIDs = append(m.StickerIDs, stickerIds...)
+	return m
+}
+
+// RemoveSticker returns a new MessageCreate with the provided sticker removed.
+func (m MessageCreate) RemoveSticker(stickerId snowflake.ID) MessageCreate {
+	m.StickerIDs = slices.DeleteFunc(slices.Clone(m.StickerIDs), func(id snowflake.ID) bool {
+		return id == stickerId
+	})
+	return m
+}
+
+// ClearStickers returns a new MessageCreate with no Sticker(s).
+func (m MessageCreate) ClearStickers() MessageCreate {
+	m.StickerIDs = []snowflake.ID{}
+	return m
+}
+
+// WithFiles returns a new MessageCreate with the provided File(s).
+func (m MessageCreate) WithFiles(files ...*File) MessageCreate {
+	m.Files = files
+	return m
+}
+
+// UpdateFile returns a new MessageCreate with the provided File at the index.
+func (m MessageCreate) UpdateFile(i int, file *File) MessageCreate {
+	if len(m.Files) > i {
+		m.Files = slices.Clone(m.Files)
+		m.Files[i] = file
+	}
+	return m
+}
+
+// AddFiles returns a new MessageCreate with the File(s) added.
+func (m MessageCreate) AddFiles(files ...*File) MessageCreate {
+	m.Files = append(m.Files, files...)
+	return m
+}
+
+// AddFile returns a new MessageCreate with a File added.
+func (m MessageCreate) AddFile(name string, description string, reader io.Reader, flags ...FileFlags) MessageCreate {
+	m.Files = append(m.Files, NewFile(name, description, reader, flags...))
+	return m
+}
+
+// RemoveFile returns a new MessageCreate with the File at the index removed.
+func (m MessageCreate) RemoveFile(i int) MessageCreate {
+	if len(m.Files) > i {
+		m.Files = slices.Delete(slices.Clone(m.Files), i, i+1)
+	}
+	return m
+}
+
+// ClearFiles returns a new MessageCreate with no File(s).
+func (m MessageCreate) ClearFiles() MessageCreate {
+	m.Files = []*File{}
+	return m
+}
+
+// WithAllowedMentions returns a new MessageCreate with the provided AllowedMentions.
+func (m MessageCreate) WithAllowedMentions(allowedMentions *AllowedMentions) MessageCreate {
+	m.AllowedMentions = allowedMentions
+	return m
+}
+
+// ClearAllowedMentions returns a new MessageCreate with no AllowedMentions.
+func (m MessageCreate) ClearAllowedMentions() MessageCreate {
+	return m.WithAllowedMentions(nil)
+}
+
+// WithMessageReference returns a new MessageCreate with the provided MessageReference to reply to.
+func (m MessageCreate) WithMessageReference(messageReference *MessageReference) MessageCreate {
+	m.MessageReference = messageReference
+	return m
+}
+
+// WithMessageReferenceByID returns a new MessageCreate with a MessageReference to the provided Message ID to reply to.
+func (m MessageCreate) WithMessageReferenceByID(messageID snowflake.ID) MessageCreate {
+	m.MessageReference = &MessageReference{
+		MessageID: &messageID,
+	}
+	return m
+}
+
+// WithFlags returns a new MessageCreate with the provided message flags.
+func (m MessageCreate) WithFlags(flags ...MessageFlags) MessageCreate {
+	m.Flags = m.Flags.Add(flags...)
+	return m
+}
+
+// AddFlags returns a new MessageCreate with the provided MessageFlags added.
+func (m MessageCreate) AddFlags(flags ...MessageFlags) MessageCreate {
+	m.Flags = m.Flags.Add(flags...)
+	return m
+}
+
+// RemoveFlags returns a new MessageCreate with the provided MessageFlags removed.
+func (m MessageCreate) RemoveFlags(flags ...MessageFlags) MessageCreate {
+	m.Flags = m.Flags.Remove(flags...)
+	return m
+}
+
+// ClearFlags returns a new MessageCreate with no MessageFlags.
+func (m MessageCreate) ClearFlags() MessageCreate {
+	return m.WithFlags(MessageFlagsNone)
+}
+
+// WithEphemeral returns a new MessageCreate with MessageFlagEphemeral added/removed.
+func (m MessageCreate) WithEphemeral(ephemeral bool) MessageCreate {
+	if ephemeral {
+		m.Flags = m.Flags.Add(MessageFlagEphemeral)
+	} else {
+		m.Flags = m.Flags.Remove(MessageFlagEphemeral)
+	}
+	return m
+}
+
+// WithIsComponentsV2 returns a new MessageCreate with MessageFlagIsComponentsV2 added/removed.
+// Once a message with the flag has been sent, it cannot be removed by editing the message.
+func (m MessageCreate) WithIsComponentsV2(isComponentV2 bool) MessageCreate {
+	if isComponentV2 {
+		m.Flags = m.Flags.Add(MessageFlagIsComponentsV2)
+	} else {
+		m.Flags = m.Flags.Remove(MessageFlagIsComponentsV2)
+	}
+	return m
+}
+
+// WithSuppressEmbeds returns a new MessageCreate with MessageFlagSuppressEmbeds added/removed.
+func (m MessageCreate) WithSuppressEmbeds(suppressEmbeds bool) MessageCreate {
+	if suppressEmbeds {
+		m.Flags = m.Flags.Add(MessageFlagSuppressEmbeds)
+	} else {
+		m.Flags = m.Flags.Remove(MessageFlagSuppressEmbeds)
+	}
+	return m
+}
+
+// WithSuppressNotifications returns a new MessageCreate with MessageFlagSuppressNotifications added/removed.
+func (m MessageCreate) WithSuppressNotifications(suppressNotifications bool) MessageCreate {
+	if suppressNotifications {
+		m.Flags = m.Flags.Add(MessageFlagSuppressNotifications)
+	} else {
+		m.Flags = m.Flags.Remove(MessageFlagSuppressNotifications)
+	}
+	return m
+}
+
+// WithPoll returns a new MessageCreate with the provided Poll.
+func (m MessageCreate) WithPoll(poll PollCreate) MessageCreate {
+	m.Poll = &poll
+	return m
+}
+
+// ClearPoll returns a new MessageCreate with no Poll.
+func (m MessageCreate) ClearPoll() MessageCreate {
+	m.Poll = nil
+	return m
 }
--- discord/message_update.go
+++ discord/message_update.go
@@ -1,6 +1,28 @@
 package discord
 
-// MessageUpdate is used to edit a Message
+import (
+	"fmt"
+	"io"
+	"slices"
+
+	"github.com/disgoorg/snowflake/v2"
+)
+
+// NewMessageUpdate returns a new MessageUpdate with no fields set.
+func NewMessageUpdate() MessageUpdate {
+	return MessageUpdate{}
+}
+
+// NewMessageUpdateV2 returns a new MessageUpdate with MessageFlagIsComponentsV2 set and no other fields set.
+func NewMessageUpdateV2(components []LayoutComponent) MessageUpdate {
+	flags := MessageFlagIsComponentsV2
+	return MessageUpdate{
+		Flags:      &flags,
+		Components: &components,
+	}
+}
+
+// MessageUpdate is used to edit a Message.
 type MessageUpdate struct {
 	Content         *string             `json:"content,omitempty"`
 	Embeds          *[]Embed            `json:"embeds,omitempty"`
@@ -16,7 +38,7 @@ type MessageUpdate struct {
 
 func (MessageUpdate) interactionCallbackData() {}
 
-// ToBody returns the MessageUpdate ready for body
+// ToBody returns the MessageUpdate ready for body.
 func (m MessageUpdate) ToBody() (any, error) {
 	if len(m.Files) > 0 {
 		for _, attachmentCreate := range parseAttachments(m.Files) {
@@ -41,4 +63,275 @@ func (m MessageUpdate) ToResponseBody(response InteractionResponse) (any, error)
 		return PayloadWithFiles(response, m.Files...)
 	}
 	return response, nil
+}
+
+// WithContent returns a new MessageUpdate with the provided content.
+func (m MessageUpdate) WithContent(content string) MessageUpdate {
+	m.Content = &content
+	return m
+}
+
+// WithContentf returns a new MessageUpdate with the formatted content.
+func (m MessageUpdate) WithContentf(content string, a ...any) MessageUpdate {
+	return m.WithContent(fmt.Sprintf(content, a...))
+}
+
+// ClearContent returns a new MessageUpdate with no content.
+func (m MessageUpdate) ClearContent() MessageUpdate {
+	return m.WithContent("")
+}
+
+// WithEmbeds returns a new MessageUpdate with the provided Embed(s).
+func (m MessageUpdate) WithEmbeds(embeds ...Embed) MessageUpdate {
+	m.Embeds = &embeds
+	return m
+}
+
+// WithEmbed returns a new MessageUpdate with the provided Embed at the index.
+func (m MessageUpdate) WithEmbed(i int, embed Embed) MessageUpdate {
+	if m.Embeds == nil {
+		m.Embeds = &[]Embed{}
+	}
+	if len(*m.Embeds) > i {
+		newEmbeds := slices.Insert(*m.Embeds, i, embed)
+		m.Embeds = &newEmbeds
+	}
+	return m
+}
+
+// AddEmbeds returns a new MessageUpdate with the provided embeds added.
+func (m MessageUpdate) AddEmbeds(embeds ...Embed) MessageUpdate {
+	if m.Embeds == nil {
+		m.Embeds = &[]Embed{}
+	}
+	newEmbeds := append(slices.Clone(*m.Embeds), embeds...)
+	m.Embeds = &newEmbeds
+	return m
+}
+
+// ClearEmbeds returns a new MessageUpdate with no embeds.
+func (m MessageUpdate) ClearEmbeds() MessageUpdate {
+	m.Embeds = &[]Embed{}
+	return m
+}
+
+// RemoveEmbed returns a new MessageUpdate with the embed at the index removed.
+func (m MessageUpdate) RemoveEmbed(i int) MessageUpdate {
+	if m.Embeds == nil {
+		m.Embeds = &[]Embed{}
+	}
+	if len(*m.Embeds) > i {
+		newEmbeds := slices.Delete(slices.Clone(*m.Embeds), i, i+1)
+		m.Embeds = &newEmbeds
+	}
+	return m
+}
+
+// WithComponents returns a new MessageUpdate with the provided LayoutComponent(s).
+func (m MessageUpdate) WithComponents(components ...LayoutComponent) MessageUpdate {
+	m.Components = &components
+	return m
+}
+
+// UpdateComponent returns a new MessageUpdate with the provided LayoutComponent at the index.
+func (m MessageUpdate) UpdateComponent(id int, component LayoutComponent) MessageUpdate {
+	if m.Components == nil {
+		return m
+	}
+	for i, cc := range *m.Components {
+		if cc.GetID() == id {
+			newComponents := slices.Clone(*m.Components)
+			newComponents[i] = component
+			m.Components = &newComponents
+			return m
+		}
+	}
+	return m
+}
+
+// AddActionRow returns a new MessageUpdate with a new ActionRowComponent containing the provided InteractiveComponent(s) added.
+func (m MessageUpdate) AddActionRow(components ...InteractiveComponent) MessageUpdate {
+	if m.Components == nil {
+		m.Components = &[]LayoutComponent{}
+	}
+	newComponents := append(slices.Clone(*m.Components), NewActionRow(components...))
+	m.Components = &newComponents
+	return m
+}
+
+// AddComponents returns a new MessageUpdate with the provided LayoutComponent(s) added.
+func (m MessageUpdate) AddComponents(containers ...LayoutComponent) MessageUpdate {
+	if m.Components == nil {
+		m.Components = &[]LayoutComponent{}
+	}
+	newComponents := append(slices.Clone(*m.Components), containers...)
+	m.Components = &newComponents
+	return m
+}
+
+// RemoveComponent returns a new MessageUpdate with the LayoutComponent with the provided ID removed.
+func (m MessageUpdate) RemoveComponent(id int) MessageUpdate {
+	if m.Components == nil {
+		return m
+	}
+	for i, cc := range *m.Components {
+		if cc.GetID() == id {
+			newComponents := slices.Delete(slices.Clone(*m.Components), i, i+1)
+			m.Components = &newComponents
+			return m
+		}
+	}
+	return m
+}
+
+// ClearComponents returns a new MessageUpdate with no LayoutComponent(s).
+func (m MessageUpdate) ClearComponents() MessageUpdate {
+	m.Components = &[]LayoutComponent{}
+	return m
+}
+
+// WithFiles returns a new MessageUpdate with the provided File(s).
+func (m MessageUpdate) WithFiles(files ...*File) MessageUpdate {
+	m.Files = files
+	return m
+}
+
+// UpdateFile returns a new MessageUpdate with the File at the index.
+func (m MessageUpdate) UpdateFile(i int, file *File) MessageUpdate {
+	if len(m.Files) > i {
+		m.Files = slices.Clone(m.Files)
+		m.Files[i] = file
+	}
+	return m
+}
+
+// AddFiles returns a new MessageUpdate with the File(s) added.
+func (m MessageUpdate) AddFiles(files ...*File) MessageUpdate {
+	m.Files = append(m.Files, files...)
+	return m
+}
+
+// AddFile returns a new MessageUpdate with a File added.
+func (m MessageUpdate) AddFile(name string, description string, reader io.Reader, flags ...FileFlags) MessageUpdate {
+	m.Files = append(m.Files, NewFile(name, description, reader, flags...))
+	return m
+}
+
+// ClearFiles returns a new MessageUpdate with no File(s).
+func (m MessageUpdate) ClearFiles() MessageUpdate {
+	m.Files = []*File{}
+	return m
+}
+
+// RemoveFile returns a new MessageUpdate with the File at the index removed.
+func (m MessageUpdate) RemoveFile(i int) MessageUpdate {
+	if len(m.Files) > i {
+		m.Files = slices.Delete(slices.Clone(m.Files), i, i+1)
+	}
+	return m
+}
+
+// RetainAttachments returns a new MessageUpdate that retains the provided Attachment(s).
+func (m MessageUpdate) RetainAttachments(attachments ...Attachment) MessageUpdate {
+	if m.Attachments == nil {
+		m.Attachments = &[]AttachmentUpdate{}
+	}
+	newAttachments := slices.Clone(*m.Attachments)
+	for _, attachment := range attachments {
+		newAttachments = append(newAttachments, AttachmentKeep{
+			ID: attachment.ID,
+		})
+	}
+	m.Attachments = &newAttachments
+	return m
+}
+
+// RetainAttachmentsByID returns a new MessageUpdate that retains the Attachment(s) with the provided IDs.
+func (m MessageUpdate) RetainAttachmentsByID(attachmentIDs ...snowflake.ID) MessageUpdate {
+	if m.Attachments == nil {
+		m.Attachments = &[]AttachmentUpdate{}
+	}
+	newAttachments := slices.Clone(*m.Attachments)
+	for _, attachmentID := range attachmentIDs {
+		newAttachments = append(newAttachments, AttachmentKeep{
+			ID: attachmentID,
+		})
+	}
+	m.Attachments = &newAttachments
+	return m
+}
+
+// WithAllowedMentions returns a new MessageUpdate with the provided AllowedMentions.
+func (m MessageUpdate) WithAllowedMentions(allowedMentions *AllowedMentions) MessageUpdate {
+	m.AllowedMentions = allowedMentions
+	return m
+}
+
+// ClearAllowedMentions returns a new MessageUpdate with no AllowedMentions.
+func (m MessageUpdate) ClearAllowedMentions() MessageUpdate {
+	return m.WithAllowedMentions(nil)
+}
+
+// WithFlags returns a new MessageUpdate with the provided MessageFlags.
+// Be careful not to override the current flags when editing messages from other users - this will result in a permission error.
+// Use WithSuppressEmbeds or AddFlags for flags like MessageFlagSuppressEmbeds.
+func (m MessageUpdate) WithFlags(flags MessageFlags) MessageUpdate {
+	m.Flags = &flags
+	return m
+}
+
+// AddFlags returns a new MessageUpdate with the provided MessageFlags added.
+func (m MessageUpdate) AddFlags(flags ...MessageFlags) MessageUpdate {
+	if m.Flags == nil {
+		m.Flags = new(MessageFlags)
+	}
+	newFlags := (*m.Flags).Add(flags...)
+	m.Flags = &newFlags
+	return m
+}
+
+// RemoveFlags returns a new MessageUpdate with the provided MessageFlags removed.
+func (m MessageUpdate) RemoveFlags(flags ...MessageFlags) MessageUpdate {
+	if m.Flags == nil {
+		m.Flags = new(MessageFlags)
+	}
+	newFlags := (*m.Flags).Remove(flags...)
+	m.Flags = &newFlags
+	return m
+}
+
+// ClearFlags returns a new MessageUpdate with no MessageFlags.
+func (m MessageUpdate) ClearFlags() MessageUpdate {
+	return m.WithFlags(MessageFlagsNone)
+}
+
+// WithSuppressEmbeds returns a new MessageUpdate with MessageFlagSuppressEmbeds added/removed.
+func (m MessageUpdate) WithSuppressEmbeds(suppressEmbeds bool) MessageUpdate {
+	if m.Flags == nil {
+		m.Flags = new(MessageFlags)
+	}
+	flags := *m.Flags
+	if suppressEmbeds {
+		flags = m.Flags.Add(MessageFlagSuppressEmbeds)
+	} else {
+		flags = m.Flags.Remove(MessageFlagSuppressEmbeds)
+	}
+	m.Flags = &flags
+	return m
+}
+
+// WithIsComponentsV2 returns a new MessageUpdate with MessageFlagIsComponentsV2 added/removed.
+// Once a message with the flag has been sent, it cannot be removed by editing the message.
+func (m MessageUpdate) WithIsComponentsV2(isComponentV2 bool) MessageUpdate {
+	if m.Flags == nil {
+		m.Flags = new(MessageFlags)
+	}
+	flags := *m.Flags
+	if isComponentV2 {
+		flags = m.Flags.Add(MessageFlagIsComponentsV2)
+	} else {
+		flags = m.Flags.Remove(MessageFlagIsComponentsV2)
+	}
+	m.Flags = &flags
+	return m
 }
