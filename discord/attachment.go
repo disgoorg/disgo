8a0c5ec9c8606c7aac54fb19064a722d6c30db57
Kshitij Anurag <230598819+kshitijanurag@users.noreply.github.com>
2026-08-11 01:30:34 +0530

    feat: attachment editing (#567)

--- discord/attachment.go
+++ discord/attachment.go
@@ -45,19 +45,65 @@ const (
 	AttachmentFlagsNone AttachmentFlags = 0
 )
 
+// AttachmentUpdate is used in Message Create and Edit requests to set which Attachment(s) are in the message and provide their metadata.
+// [AttachmentKeep] to retain (and optionally edit) an existing Attachment
+// [AttachmentCreate] to describe a newly uploaded file
 type AttachmentUpdate interface {
 	attachmentUpdate()
 }
 
+// NewAttachmentKeep returns a new AttachmentKeep with the provided ID and no other fields set.
+func NewAttachmentKeep(id snowflake.ID) AttachmentKeep {
+	return AttachmentKeep{ID: id}
+}
+
+// AttachmentKeep is used to retain an existing Attachment when editing a Message.
+//
+// When editing, you must provide the ID of each file to keep, and you can optionally update the Description and IsSpoiler fields of existing attachments.
 type AttachmentKeep struct {
-	ID snowflake.ID `json:"id"`
+	ID          snowflake.ID `json:"id"`
+	Description *string      `json:"description,omitempty"`
+	IsSpoiler   *bool        `json:"is_spoiler,omitempty"`
 }
 
 func (AttachmentKeep) attachmentUpdate() {}
 
+// WithDescription returns a new AttachmentKeep with the provided description.
+func (a AttachmentKeep) WithDescription(description string) AttachmentKeep {
+	a.Description = &description
+	return a
+}
+
+// WithSpoiler returns a new AttachmentKeep with the provided spoiler setting.
+func (a AttachmentKeep) WithSpoiler(spoiler bool) AttachmentKeep {
+	a.IsSpoiler = &spoiler
+	return a
+}
+
+// NewAttachmentCreate returns a new AttachmentCreate with the provided ID and no other fields set.
+//
+// ID must match the n in files[n] of the file this AttachmentCreate describes.
+func NewAttachmentCreate(id int) AttachmentCreate {
+	return AttachmentCreate{ID: id}
+}
+
+// AttachmentCreate is used to describe the metadata of a newly uploaded file when creating or editing a Message.
 type AttachmentCreate struct {
 	ID          int    `json:"id"`
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
+	IsSpoiler   *bool  `json:"is_spoiler,omitempty"`
 }
 
 func (AttachmentCreate) attachmentUpdate() {}
+
+// WithDescription returns a new AttachmentCreate with the provided description.
+func (a AttachmentCreate) WithDescription(description string) AttachmentCreate {
+	a.Description = description
+	return a
+}
+
+// WithSpoiler returns a new AttachmentCreate with the provided spoiler setting.
+func (a AttachmentCreate) WithSpoiler(spoiler bool) AttachmentCreate {
+	a.IsSpoiler = &spoiler
+	return a
+}
